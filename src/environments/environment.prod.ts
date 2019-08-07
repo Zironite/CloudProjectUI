e07ecf8649ef58a91297ee5d96b6182551e73ae7
liron <lironpro14@gmail.com>
2019-08-07 21:34:25 +0300

    now using http url for server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-100-24-61-182.compute-1.amazonaws.com:55555'
+  apiUrl: 'http://ec2-100-24-61-182.compute-1.amazonaws.com'
 };
